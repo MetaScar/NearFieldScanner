fbd611f5acab7361d9871185603855da62baca29
jacob <jacob@10.180.5.26>
2024-07-03 10:31:57 -0600

    In move.m   Modify parmsTable data collection to center at probe center rather than machine absolute position.   Add function to move machine X, code Z (untested)   Add function to check if input is number (locally tested)

--- move.m
+++ move.m
@@ -62,7 +62,7 @@ for i = 1:size(Y_pos_str)
         % Grab an S-Parameter Object
         % Note that anything can go in the second parameter of the function
         S = pna.get("S2P", "");
-        paramsTable(tablePos, :) = {X_position(j), Y_position(i), S};
+        paramsTable(tablePos, :) = {X_position(j)-x_center, Y_position(i)-y_center, S};
         tablePos = tablePos + 1;
         % if going the odd route,
         if odd
@@ -301,3 +301,47 @@ while 1
 end
 end
 
+% Prompt user to move to desired machine X position, code Z position. 
+% User input will change the relative position of the machine X position. 
+% Code uses recursion. 
+% Escape condition is an input of 0.
+% 2024-07-03: Untested
+function ZPosition(x)
+z_min = -898.8;
+z_max = -446.8;
+
+a = input("This code will move the probe closer or futher away from the AUT. Positive numbers will move it closer, negative further. Enter 0 to stop movement controls.\n" + ...
+    "Please provide input (mm): ", "s");
+
+inputNumCheck(a);
+
+numInput = str2double(a);
+
+if numInput == 0
+    return
+end
+
+while (numInput > z_max) || (numInput < z_min)
+    a = input("Input goes beyond machine limits! Please try again: ", "s");
+    inputNumCheck(a);
+    numInput = str2double(a);
+end
+
+command = "G91 X" + numInput;
+writeline(x, command);
+WaitMotionComplete(x);
+ZPosition(x);
+
+end
+
+% input response (string), check whether string only has numbers. If it
+% doesn't will prompt user until it is a number
+% Note: requires original prompt for number to recieve string.
+% Ex: input("...", "s") rather than input("...")
+% 2024-07-03 Locally Tested, not Machine Tested
+function a = inputNumCheck(response)
+while isnan(str2double(response))
+    response = input("Input is not a number!  Please try again: ", "s");
+end
+a = response;
+end
